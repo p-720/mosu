d1c9f70ccd0901e3ddf3e3633fea30b5170c7ed5
patriot720 <cerkin-3@yandex.ru>
2026-09-06 12:50:57 +0300

    cleanup

--- osu.Game.Rulesets.MOsu/Extensions/OsuGameExtensions.cs
+++ osu.Game.Rulesets.MOsu/Extensions/OsuGameExtensions.cs
@@ -16,26 +16,17 @@ public static class OsuGameExtensions
 
     public static Container? GetWaveOverlayPlacementContainer(this OsuGame game)
     {
-        // will place the container where components of an WaveOverlayContainer type exist
         return game.getBasePlacementContainer()?.Children.OfType<Container>().FirstOrDefault(c => c.Children.OfType<WaveOverlayContainer>().Any());
     }
 
     public static FillFlowContainer? GetToolbarContainer(this OsuGame game)
     {
-        // will place the container where components of an WaveOverlayContainer type exist
         var grid = game.Toolbar.Children.OfType<GridContainer>().FirstOrDefault();
 
-        // 2. Get the "Right buttons" container.
-        // In the source code, Content is a 2D array. The right buttons are Row 0, Column 2.
         var rightButtonContainer = grid?.Content[0][2] as Container;
 
-        // 3. Get the FillFlowContainer from inside that container
         var rightButtonsFlow = rightButtonContainer?.Children.OfType<FillFlowContainer>().FirstOrDefault();
 
         return rightButtonsFlow;
-        // return game.getBasePlacementContainer()?.Children.OfType<Container>().FirstOrDefault(c => c.Children.OfType<WaveOverlayContainer>().Any());
     }
-
-    public static SettingsOverlay? GetSettingsOverlay(this OsuGame game)
-        => game.getBasePlacementContainer()?.ChildrenOfType<SettingsOverlay>().FirstOrDefault();
 }
